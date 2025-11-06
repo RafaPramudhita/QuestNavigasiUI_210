ed77fd82ed73b1b27c097e5776250f29d2a1b667
RafaPramudhita <rafapram360@gmail.com>
2025-11-06 15:48:18 +0700

    4.Deklarasi NavHost dan startDestination

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -22,6 +22,14 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
 
+        }
+    }
 }
 
