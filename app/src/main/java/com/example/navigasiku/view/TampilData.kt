31b3f716042979ff0a9e0f04ec3b031ec9c1c34d
RafaPramudhita <rafapram360@gmail.com>
2025-11-06 15:58:57 +0700

    14.membuat AppBar halaman detail

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -25,5 +25,22 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
     )
 
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
 
+
+        }
+    }
 }
