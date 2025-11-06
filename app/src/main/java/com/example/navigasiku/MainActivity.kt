2b002b8669416c57b784e45dc6461aa3fdc93a51
RafaPramudhita <rafapram360@gmail.com>
2025-11-06 16:03:31 +0700

    19. Aktifkan edge-to-edge agar konten full screen

--- app/src/main/java/com/example/navigasiku/MainActivity.kt
+++ app/src/main/java/com/example/navigasiku/MainActivity.kt
@@ -10,7 +10,7 @@ import com.example.navigasiku.DataApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
 
     }
 }
