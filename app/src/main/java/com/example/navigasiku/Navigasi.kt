2d00eb51fdb67daea3f1dbc8c05f052b780d9162
RafaPramudhita <rafapram360@gmail.com>
2025-11-06 15:46:16 +0700

    1.Import Navigation-Compose & Compose UI

---
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -0,0 +1,15 @@
+package com.example.navigasiku
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
+import com.example.navigasiku.view.FormIsian
+import com.example.navigasiku.view.TampilData
+
+
