05c4c91ac42de4f88e41e92675c641582f283420
RafaPramudhita <rafapram360@gmail.com>
2025-11-06 15:51:56 +0700

    7.Import Material3 & resource Compose

---
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -0,0 +1,15 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.navigasiku.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasiku.R
+
